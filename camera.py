07a4f38bf8a6ab263177c86ef057680bb6f9fbb7
filipwicha <5021567@gmail.com>
2019-04-03 16:33:09 +0200

    commit

---
+++ camera.py
@@ -0,0 +1,19 @@
+import cv2
+
+cv2.namedWindow("preview")
+vc = cv2.VideoCapture(0)
+
+rval, frame = vc.read()
+
+while True:
+
+  if frame is not None:
+     cv2.imshow("preview", frame)
+  rval, frame = vc.read()
+
+  if cv2.waitKey(1) & 0xFF == ord('q'):
+     break
+
+vc.release()
+cv2.waitKey(0)
+cv2.destroyAllWindows()
