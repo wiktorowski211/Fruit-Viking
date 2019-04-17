85fd07125f2c6d2ab581c704084a1bccb334690a
filipwicha <5021567@gmail.com>
2019-04-17 16:37:02 +0200

    updated camera module to catch colors

--- camera/camera.py
+++ camera/camera.py
@@ -37,9 +37,9 @@ while(1):
     # Bitwise-AND mask and original image
     res = cv2.bitwise_and(frame,frame, mask= mask)
 
-    #cv2.imshow('frame', frame)
+    cv2.imshow('frame', frame)
     #cv2.imshow('mask', mask)
-    cv2.imshow('res', res)
+    #cv2.imshow('res', res)
     k = cv2.waitKey(5) & 0xFF
     if k == 27:
         break
