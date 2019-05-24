bce077cb0f47d52c66835b7a0f6166fa4465b5e3
Rzar8767 <33533895+Rzar8767@users.noreply.github.com>
2019-05-24 14:26:32 +0200

    Add experimental strawberry target

--- src/states/level1.py
+++ src/states/level1.py
@@ -1,18 +1,32 @@
 from .state import State
+from src.targets import Strawberry
+from src.colors import gray
+import pygame
 
 
-class LevelState1:
+class LevelState1(State):
 
     def __init__(self, *args, **kwargs):
         State.__init__(self, *args, **kwargs)
         self.active = True
 
+        self.target1 = Strawberry((200, 200), self.screen, False)
 
     def render(self):
-        pass
+        rects = []
+
+        self.screen.fill(gray)
+        rect_a, rect_b = self.target1.render()
+        rects.append(rect_a)
+        rects.append(rect_b)
+
+        return rects
 
     def tick(self, dt):
-        pass
+        self.target1.update(dt, (300, 300), 10)
 
     def event(self, event):
-        pass
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                print("Go back to level selection")
+                self._game.remove_top_state()
---
+++ src/targets/strawberry.py
@@ -0,0 +1,11 @@
+from .target import Target
+import src.resources as res
+
+
+class Strawberry(Target):
+    #    def __init__(self, image, pos, screen, debug: bool = True):
+
+    def __init__(self, pos, screen, debug: bool = False):
+        strawberry = res.gfx('strawberry.png', convert=True)
+        Target.__init__(self, strawberry, pos, screen, debug)
+        self.velocity = (100., 100.)
