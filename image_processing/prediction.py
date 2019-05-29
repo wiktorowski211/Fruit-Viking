6a5e46048b81a514a0949b50e9e3b9e8738cd1d0
marcin.wiktorowski <marcin.wiktorowski@appunite.com>
2019-05-29 17:29:09 +0200

    added prediction integration

--- image_processing/prediction.py
+++ image_processing/prediction.py
@@ -1,36 +1,44 @@
 from image_processing.camera import Camera
-import time
+from math import sqrt
 
 
 class Prediction:
     def __init__(self):
-        # loop
-        self.frequency = 30
         self.last_point = 0, 0
-        self.last_time = time.time()
-        self.last_velocity = 0
+        self.predicted_point = 0, 0
+        self.last_distance = 0
 
-        self.vector_x = 0
-        self.vector_y = 0
+    def is_point_valid(self, point, distance_toleration):
+        distance = self.get_distance(point, self.predicted_point)
+        if distance < self.last_distance + distance_toleration:
+            return True
+        else:
+            return False
 
-    def predict(self, point):
-        # new_time = time.time()
+    def get_distance(self, point1, point2):
+        x1, y1 = point1
+        x0, y0 = point2
+        return sqrt((x1 - x0) ** 2 + (y1 - y0) ** 2)
 
+    def predict_next_point(self, point):
         x1, y1 = point
         x0, y0 = self.last_point
 
-        self.vector_x = x1 - x0
-        self.vector_y = y1 - y0
+        self.last_distance = self.get_distance(point, self.last_point)
+
+        x1 += x1 - x0
+        y1 += y1 - y0
 
         self.last_point = point
+        self.predicted_point = x1, y1
 
 
 if __name__ == '__main__':
     camera = Camera(1280, 720)
 
     pr = Prediction()
-    pr.predict((5, 5))
-    pr.predict((10, 10))
-    pr.predict((15, 15))
-    pr.predict((20, 22))
-    pr.predict((25, 29))
+    pr.predict_next_point((10, 20))
+    pr.predict_next_point((8, 18))
+    pr.predict_next_point((6, 16))
+    pr.predict_next_point((20, 22))
+    pr.predict_next_point((25, 29))
